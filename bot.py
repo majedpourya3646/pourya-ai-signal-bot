700c39404cccaf6e88a9508197bd456063b0a263
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-11 21:22:27 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+from market import get_market_data
+from signal_engine import analyze_market
+from telegram_sender import send_message
+
+
+SYMBOLS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "XRPUSDT",
+    "DOGEUSDT"
+]
+
+
+def run_bot():
+
+    for symbol in SYMBOLS:
+
+        try:
+            df = get_market_data(symbol)
+
+            result = analyze_market(df)
+
+            if result["signal"] != "WAIT":
+
+                message = (
+                    f"🚨 Crypto Signal\n\n"
+                    f"🪙 {symbol}\n"
+                    f"📈 Action: {result['signal']}\n\n"
+                    f"💰 Entry: {result['price']}\n"
+                    f"🎯 TP: {result['tp']}\n"
+                    f"🛑 SL: {result['sl']}\n\n"
+                    f"⭐ Confidence: {result['confidence']}%"
+                )
+
+                send_message(message)
+
+        except Exception as e:
+            print(symbol, e)
+
+
+if __name__ == "__main__":
+    run_bot()
