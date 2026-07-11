c6d0ae9dba0b8ff1f12728c2a6aa30024e94aebb
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-12 01:34:46 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+import os
+
+print("FILES:")
+print(os.listdir())
+
 from backtest import run_backtest
 
 run_backtest("BTCUSDT")
-run_backtest("ETHUSDT")
-run_backtest("SOLUSDT")
-run_backtest("XRPUSDT")
-run_backtest("DOGEUSDT")
