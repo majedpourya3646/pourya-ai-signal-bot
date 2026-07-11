2fd8ad46ccd76d0931df3c1824c1db168146342e
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-11 21:19:06 +0330

    Create market.py

---
+++ market.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+
+
+def get_market_data(symbol="BTCUSDT", interval="15m", limit=100):
+
+    url = "https://api.binance.com/api/v3/klines"
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+
+    response = requests.get(url, params=params)
+
+    data = response.json()
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "close_time",
+            "quote_volume",
+            "trades",
+            "taker_buy_base",
+            "taker_buy_quote",
+            "ignore"
+        ]
+    )
+
+    df["close"] = df["close"].astype(float)
+    df["volume"] = df["volume"].astype(float)
+
+    return df
