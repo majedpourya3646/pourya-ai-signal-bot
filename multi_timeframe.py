1e80b78196c42480d628f543480ce7e1efeb0650
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-12 10:55:46 +0330

    Update multi_timeframe.py

--- multi_timeframe.py
+++ multi_timeframe.py
@@ -1,15 +1,12 @@
 def check_timeframes(df_15m, df_1h, df_4h):
     score = 0
 
-    # روند کوتاه مدت
     if df_15m["close"].iloc[-1] > df_15m["close"].iloc[-10]:
         score += 1
 
-    # روند میان مدت
     if df_1h["close"].iloc[-1] > df_1h["close"].iloc[-10]:
         score += 1
 
-    # روند اصلی
     if df_4h["close"].iloc[-1] > df_4h["close"].iloc[-10]:
         score += 1
 
@@ -25,3 +22,19 @@ def check_timeframes(df_15m, df_1h, df_4h):
 
     else:
         return "NO TRADE"
+
+
+
+def analyze_symbol(symbol):
+    """
+    تحلیل اصلی هر ارز
+    فعلا نسخه تستی
+    """
+
+    result = {
+        "symbol": symbol,
+        "signal": "WAIT",
+        "score": 0
+    }
+
+    return result
