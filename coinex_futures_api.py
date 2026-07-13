4c8c82bfe3a0796db9c46012560eeb6131d498ed
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-13 21:56:12 +0330

    Update coinex_futures_api.py

--- coinex_futures_api.py
+++ coinex_futures_api.py
@@ -1,1 +1,14 @@
+def get_futures_balance(self):
 
+    return self._request(
+        "GET",
+        "/assets/futures/balance"
+    )
+
+
+def get_futures_positions(self):
+
+    return self._request(
+        "GET",
+        "/futures/pending-position"
+    )
