72e29cc6bd1130130c75cbe68e8f2dfef14aad93
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-13 20:39:24 +0330

    Update coinex.py

--- config/coinex.py
+++ config/coinex.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BASE_URL = "https://api.coinex.com/v2"
+
+COINEX_API_KEY = os.getenv("COINEX_API_KEY")
+
+COINEX_SECRET_KEY = os.getenv("COINEX_SECRET_KEY")
+
+MARKET_TYPE = "FUTURES"
+
+REQUEST_TIMEOUT = 30
+
+MAX_RETRIES = 3
