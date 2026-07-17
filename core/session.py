f57a2f704f8745bd217f9d9678c904d1822698cc
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-17 15:25:45 +0330

    Update session.py

--- core/session.py
+++ core/session.py
@@ -1,26 +1,24 @@
-import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-
-COINEX_API_KEY = os.getenv("COINEX_API_KEY")
-COINEX_SECRET_KEY = os.getenv("COINEX_SECRET_KEY")
-
-BASE_URL = "https://api.coinex.com/v2"
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
-session = requests.Session()
+class Session(requests.Session):
+    def __init__(self):
+        super().__init__()
+
+        self.timeout = 15
+
+        retries = Retry(
+            total=3,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504],
+        )
+
+        adapter = HTTPAdapter(max_retries=retries)
+
+        self.mount("https://", adapter)
+        self.mount("http://", adapter)
 
 
-def test_connection():
-    if not COINEX_API_KEY or not COINEX_SECRET_KEY:
-        return {
-            "status": "error",
-            "message": "API keys not found"
-        }
-
-    return {
-        "status": "ok",
-        "message": "CoinEx API keys loaded"
-    }
+session = Session()
