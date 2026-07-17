544655edcff13595e3214b020f3284709db55c83
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-17 20:17:56 +0330

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,11 +1,58 @@
 import logging
+import os
 
-logging.basicConfig(
 
-    level=logging.INFO,
-
-    format="%(asctime)s | %(levelname)s | %(message)s"
-
+os.makedirs(
+    "logs",
+    exist_ok=True
 )
 
-logger = logging.getLogger("PouryaTraderAI")
+
+LOG_FILE = "logs/bot.log"
+
+
+
+logger = logging.getLogger(
+    "PouryaTraderAI"
+)
+
+
+logger.setLevel(
+    logging.INFO
+)
+
+
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(message)s"
+)
+
+
+
+if not logger.handlers:
+
+
+    file_handler = logging.FileHandler(
+        LOG_FILE,
+        encoding="utf-8"
+    )
+
+    file_handler.setFormatter(
+        formatter
+    )
+
+
+    console_handler = logging.StreamHandler()
+
+    console_handler.setFormatter(
+        formatter
+    )
+
+
+    logger.addHandler(
+        file_handler
+    )
+
+    logger.addHandler(
+        console_handler
+    )
