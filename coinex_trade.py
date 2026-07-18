658fe9c5232a2f260cf65c7f1fd2a7b8cccd8f7a
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-18 19:54:00 +0330

    Update coinex_trade.py

--- coinex_trade.py
+++ coinex_trade.py
@@ -1,93 +1,54 @@
 import json
 
-
 from config import (
     PAPER_TRADING,
-    ORDER_TYPE
+    ORDER_TYPE,
 )
 
-
 from coinex_api import coinex
-
 from core.logger import logger
 
 
-
 class CoinExTrade:
-
-
 
     def create_order(
         self,
         market,
         side,
-        amount,
-        price=None
+        amount
     ):
 
-
-        logger.info(
-            f"PAPER_TRADING = {PAPER_TRADING}"
-        )
-
-
+        logger.info(f"PAPER_TRADING = {PAPER_TRADING}")
 
         if PAPER_TRADING:
-
 
             logger.info(
                 f"PAPER ORDER {side} {market} qty={amount}"
             )
 
-
             return {
-
-                "code":0,
-
-                "message":"Paper Trading",
-
-                "data":{
-
-                    "market":market,
-
-                    "side":side,
-
-                    "amount":amount,
-
-                    "order_id":"PAPER"
-
+                "code": 0,
+                "message": "Paper Trading",
+                "data": {
+                    "market": market,
+                    "side": side,
+                    "amount": amount,
+                    "order_id": "PAPER_ORDER"
                 }
-
             }
-
-
 
         logger.info(
             f"REAL ORDER {side} {market} qty={amount}"
         )
 
-
-
         result = coinex.create_futures_order(
-
             market=market,
-
             side=side,
-
             amount=amount,
-
-            order_type=ORDER_TYPE,
-
-            price=price
-
+            order_type=ORDER_TYPE
         )
 
-
-
-        logger.info(
-            "ORDER RESULT:"
-        )
-
+        logger.info("ORDER RESULT:")
 
         logger.info(
             json.dumps(
@@ -97,11 +58,7 @@ class CoinExTrade:
             )
         )
 
-
         return result
-
-
-
 
 
     def open_long(
@@ -110,19 +67,11 @@ class CoinExTrade:
         quantity
     ):
 
-
         return self.create_order(
-
             market=symbol,
-
             side="buy",
-
             amount=quantity
-
         )
-
-
-
 
 
     def open_short(
@@ -131,19 +80,31 @@ class CoinExTrade:
         quantity
     ):
 
-
         return self.create_order(
-
             market=symbol,
-
             side="sell",
-
             amount=quantity
-
         )
 
 
+    def close_position(
+        self,
+        symbol,
+        side,
+        quantity
+    ):
 
+        close_side = (
+            "sell"
+            if side.lower() == "buy"
+            else "buy"
+        )
+
+        return self.create_order(
+            market=symbol,
+            side=close_side,
+            amount=quantity
+        )
 
 
 coinex_trade = CoinExTrade()
