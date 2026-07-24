7a8cf0e23af340ee9d4095308e8c13a4f985600a
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-24 16:16:25 +0330

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,8 @@
 # core/__init__.py
 
-from core.market_scanner import *
-from core.market_ranker import *
-from core.market_cache import *
-from core.pump_detector import *
-from core.scanner_service import *
-from core.scanner_report import *
+"""
+Pourya Trader AI Core Package
+"""
+
+__version__ = "2.0.0"
+__author__ = "Pourya Majed"
