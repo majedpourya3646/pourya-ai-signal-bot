92e7580ee8844e5b2dcd2dc86287612cca121038
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-25 23:56:41 +0330

    Update market_scheduler.py

--- core/market_scheduler.py
+++ core/market_scheduler.py
@@ -2,42 +2,72 @@
 
 import time
 
+from core.final_engine import (
+    run_final_engine
+)
+
+from core.config_manager import (
+    get_setting
+)
+
 from core.logger import logger
 
 
-DEFAULT_INTERVAL = 300
 
+def start_scheduler():
 
+    try:
 
-def run_scheduler(
-    task,
-    interval=DEFAULT_INTERVAL
-):
-
-    logger.info(
-        "MARKET SCHEDULER STARTED"
-    )
-
-
-    while True:
-
-
-        try:
-
-
-            task()
-
-
-
-        except Exception as e:
-
-
-            logger.exception(
-                e
-            )
-
-
-
-        time.sleep(
-            interval
+        logger.info(
+            "MARKET SCHEDULER STARTED"
         )
+
+
+        while True:
+
+
+            try:
+
+                result = run_final_engine()
+
+
+                logger.info(
+                    f"SCHEDULER RESULT: {result}"
+                )
+
+
+
+                interval = get_setting(
+
+                    "scheduler_interval",
+
+                    300
+
+                )
+
+
+                time.sleep(
+                    interval
+                )
+
+
+
+            except Exception as e:
+
+                logger.exception(e)
+
+                time.sleep(
+                    60
+                )
+
+
+
+    except Exception as e:
+
+        logger.exception(e)
+
+
+
+if __name__ == "__main__":
+
+    start_scheduler()
