55f7b18af35e1f2b1150b2953ce380b6faed770a
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-25 23:41:28 +0330

    Update safe_runner.py

--- core/safe_runner.py
+++ core/safe_runner.py
@@ -1,50 +1,24 @@
 # core/safe_runner.py
 
-import time
-
-from core.error_handler import (
-    handle_error
-)
-
 from core.logger import logger
 
 
-
 def run_safe(
-    task,
-    interval=300,
-    name=""
+    function,
+    *args,
+    **kwargs
 ):
 
-    logger.info(
-        f"SAFE RUNNER STARTED: {name}"
-    )
+    try:
 
-
-
-    while True:
-
-
-        try:
-
-
-            task()
-
-
-
-        except Exception as e:
-
-
-            handle_error(
-
-                e,
-
-                name
-
-            )
-
-
-
-        time.sleep(
-            interval
+        return function(
+            *args,
+            **kwargs
         )
+
+
+    except Exception as e:
+
+        logger.exception(e)
+
+        return None
