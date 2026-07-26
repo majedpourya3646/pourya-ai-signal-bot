cd649fbaf11c04885477620481e17190743e938a
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-26 09:26:10 +0330

    Update trading_loop.py

--- core/trading_loop.py
+++ core/trading_loop.py
@@ -2,66 +2,92 @@
 
 import time
 
-from core.final_engine import (
-    run_final_engine
+from core.trading_controller import (
+    run_trading_cycle
 )
+
+from core.position_manager import (
+    check_tp_sl
+)
+
+from core.logger import logger
 
 from core.config_manager import (
     get_setting
 )
 
-from core.logger import logger
-
 
 
 def run_loop():
 
-    logger.info(
-        "TRADING LOOP STARTED"
-    )
+    try:
 
-
-    while True:
-
-        try:
-
-            result = run_final_engine()
-
-
-            logger.info(
-                f"LOOP RESULT: {result}"
-            )
-
-
-            interval = get_setting(
-                "loop_interval",
-                60
-            )
-
-
-            time.sleep(
-                interval
-            )
+        logger.info(
+            "TRADING LOOP STARTED"
+        )
 
 
 
-        except KeyboardInterrupt:
-
-            logger.info(
-                "TRADING LOOP STOPPED BY USER"
-            )
-
-            break
+        while True:
 
 
+            try:
 
-        except Exception as e:
 
-            logger.exception(e)
+                closed = check_tp_sl()
 
-            time.sleep(
-                30
-            )
+
+                if closed:
+
+                    logger.info(
+                        f"CLOSED POSITIONS: {closed}"
+                    )
+
+
+
+                trades = run_trading_cycle()
+
+
+
+                if trades:
+
+                    logger.info(
+                        f"EXECUTED TRADES: {trades}"
+                    )
+
+
+
+                interval = get_setting(
+
+                    "trading_interval",
+
+                    300
+
+                )
+
+
+
+                time.sleep(
+                    interval
+                )
+
+
+
+            except Exception as e:
+
+                logger.exception(e)
+
+                time.sleep(
+                    60
+                )
+
+
+
+    except Exception as e:
+
+        logger.exception(e)
+
+
 
 
 
