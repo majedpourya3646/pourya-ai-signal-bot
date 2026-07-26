f6138bfae82405886357454951eb5bc5a3315d61
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-26 20:11:32 +0330

    Create system_runner.py

---
+++ core/system_runner.py
@@ -0,0 +1,71 @@
+# core/system_runner.py
+
+from core.system_initializer import (
+    startup
+)
+
+from core.scheduler import (
+    start_all_services
+)
+
+from core.telegram_notifier import (
+    notify_system
+)
+
+from core.logger import logger
+
+
+
+
+
+def run():
+
+    try:
+
+        logger.info(
+            "STARTING POURYA TRADER AI"
+        )
+
+
+
+        if not startup():
+
+            notify_system(
+                "❌ System startup failed."
+            )
+
+            return False
+
+
+
+        notify_system(
+            "✅ Pourya Trader AI is now online."
+        )
+
+
+
+        start_all_services()
+
+
+
+        return True
+
+
+
+    except Exception as e:
+
+        logger.exception(e)
+
+        notify_system(
+            f"❌ Critical Error:\n{e}"
+        )
+
+        return False
+
+
+
+
+
+if __name__ == "__main__":
+
+    run()
