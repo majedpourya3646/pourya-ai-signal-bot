6b3d251e06016bdbc1cee9d972d027a9f79edfc0
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-26 09:14:45 +0330

    Update safe_runner.py

--- core/safe_runner.py
+++ core/safe_runner.py
@@ -1,24 +1,71 @@
 # core/safe_runner.py
 
+import traceback
+
+from core.system_health import (
+    is_system_ready
+)
+
 from core.logger import logger
 
 
-def run_safe(
-    function,
+
+def safe_execute(
+    func,
     *args,
     **kwargs
 ):
 
     try:
 
-        return function(
+        if not is_system_ready():
+
+            logger.error(
+                "SYSTEM NOT READY"
+            )
+
+            return None
+
+
+
+        return func(
             *args,
             **kwargs
         )
 
 
+
     except Exception as e:
 
-        logger.exception(e)
+        logger.error(
+            str(e)
+        )
+
+        logger.error(
+            traceback.format_exc()
+        )
 
         return None
+
+
+
+
+
+def safe_loop(
+    func,
+    interval
+):
+
+    import time
+
+
+
+    while True:
+
+        safe_execute(
+            func
+        )
+
+        time.sleep(
+            interval
+        )
