da9e4e57b0828ac7bccfe9b593d61fc1f25f7270
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-26 21:03:12 +0330

    Update market_discovery.py

--- core/market_discovery.py
+++ core/market_discovery.py
@@ -1,8 +1,27 @@
 # core/market_discovery.py
 
+from config import (
+    BASE_URL,
+    MARKET_TYPE
+)
+
 from core.session import session
 
 from core.logger import logger
+
+
+
+
+
+if MARKET_TYPE == "FUTURES":
+
+    MARKET_URL = BASE_URL + "/futures/ticker"
+
+else:
+
+    MARKET_URL = BASE_URL + "/spot/ticker"
+
+
 
 
 
@@ -10,13 +29,63 @@ def discover_markets():
 
     try:
 
-        if not session:
+        response = session.get(
+            MARKET_URL,
+            timeout=20
+        )
+
+
+        logger.info(
+            f"MARKET DISCOVERY URL: {MARKET_URL}"
+        )
+
+
+        logger.info(
+            f"MARKET DISCOVERY STATUS: {response.status_code}"
+        )
+
+
+        data = response.json()
+
+
+
+        if data.get(
+            "code"
+        ) != 0:
+
+            logger.error(
+                data
+            )
 
             return []
 
 
 
-        return []
+        markets = data.get(
+            "data",
+            []
+        )
+
+
+
+        if not markets:
+
+            return []
+
+
+
+        logger.info(
+            f"DISCOVERED MARKETS: {len(markets)}"
+        )
+
+
+        logger.info(
+            f"MARKET SAMPLE: {markets[0]}"
+        )
+
+
+
+        return markets
 
 
 
