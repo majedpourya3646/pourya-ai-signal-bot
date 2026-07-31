eef54b884c1428217022cbef22ecd8d98510a27c
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-31 19:56:33 +0330

    Update session.py

--- core/session.py
+++ core/session.py
@@ -8,9 +8,15 @@ from urllib3.util.retry import Retry
 
 from core.logger import logger
 
+from config import (
+    MAX_RETRIES,
+    REQUEST_TIMEOUT
+)
 
 
-REQUEST_TIMEOUT = 20
+
+
+
 
 
 
@@ -18,49 +24,76 @@ def create_session():
 
     try:
 
+
         session = requests.Session()
+
+
 
 
 
         retry_strategy = Retry(
 
-            total=3,
+            total=MAX_RETRIES,
 
             backoff_factor=1,
 
             status_forcelist=[
+
                 429,
+
                 500,
+
                 502,
+
                 503,
+
                 504
+
             ],
 
             allowed_methods=[
+
                 "GET",
+
                 "POST"
+
             ]
 
         )
 
 
 
+
+
         adapter = HTTPAdapter(
+
             max_retries=retry_strategy
+
         )
 
 
 
+
+
         session.mount(
+
             "https://",
+
             adapter
+
         )
+
 
 
         session.mount(
+
             "http://",
+
             adapter
+
         )
+
+
 
 
 
@@ -70,12 +103,68 @@ def create_session():
 
     except Exception as e:
 
+
         logger.exception(e)
 
-        return None
+
+        return requests.Session()
+
+
+
+
+
 
 
 
 
 
 session = create_session()
+
+
+
+
+
+
+
+def request(
+    method,
+    url,
+    **kwargs
+):
+
+    try:
+
+
+        kwargs.setdefault(
+
+            "timeout",
+
+            REQUEST_TIMEOUT
+
+        )
+
+
+
+        response = session.request(
+
+            method,
+
+            url,
+
+            **kwargs
+
+        )
+
+
+
+        return response
+
+
+
+    except Exception as e:
+
+
+        logger.exception(e)
+
+
+        return None
