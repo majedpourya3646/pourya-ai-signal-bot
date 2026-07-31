b3f7df9086952d1874248a520322e7b7a370c509
majedpourya3646 <majed.pourya3646@gmail.com>
2026-07-31 19:47:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,43 @@
 # main.py
 
-import time
-
-from core.main_engine import (
-    run_main_engine
-)
-
-from core.trading_loop import (
-    run_trading_loop
-)
-
-from core.telegram_bot_manager import (
-    process_updates
-)
-
-from core.report_scheduler import (
-    run_report_scheduler
-)
-
-from core.monthly_scheduler import (
-    run_monthly_scheduler
-)
-
-from core.health_monitor import (
-    run_health_monitor
-)
+import sys
 
 from core.logger import logger
 
-import threading
-
-
-
-def start_services():
-
-    services = [
-
-        run_main_engine,
-
-        run_trading_loop,
-
-        process_updates,
-
-        run_report_scheduler,
-
-        run_monthly_scheduler,
-
-        run_health_monitor
-
-    ]
-
-
-
-    for service in services:
-
-
-        thread = threading.Thread(
-
-            target=service
-
-        )
-
-
-        thread.daemon = True
-
-
-        thread.start()
-
-
-
-        logger.info(
-
-            f"SERVICE STARTED: {service.__name__}"
-
-        )
+from bot import (
+    main as start_bot
+)
 
 
 
 
-def main():
+
+def run():
 
     try:
 
-
         logger.info(
-
-            "🚀 POURYA TRADER AI STARTING"
-
+            "STARTING POURYA TRADER AI"
         )
 
-
-
-        start_services()
-
-
-
-        while True:
-
-
-            time.sleep(
-                60
-            )
-
-
+        start_bot()
 
     except KeyboardInterrupt:
 
-
-        logger.info(
-
-            "SYSTEM STOPPED BY USER"
-
+        logger.warning(
+            "APPLICATION STOPPED BY USER"
         )
-
-
 
     except Exception as e:
 
+        logger.exception(e)
 
-        logger.exception(
-            e
-        )
+        sys.exit(1)
+
 
 
 
 
 if __name__ == "__main__":
 
-
-    main()
+    run()
